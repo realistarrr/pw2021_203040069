883bc946e4a1c265cd66699e44a05d8be292cda2
realistarrr <75973517+realistarrr@users.noreply.github.com>
2021-03-18 11:40:27 +0700

    menyelesaikan tugas 3 praktikum pw

---
+++ praktikum/tugas3/latihan3a.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Nama    : Religi Realista Esthetika
+NRP     : 203040069
+Shift   : Praktikum PW Jumat 10.00
+*/
+?>
+
+
+
+<?php
+    $kata = [ "ada", "abel", "men", "pung", "nilai" ];
+        echo "Array $kata[0]lah suatu vari$kata[1] yang dapat $kata[2]am$kata[3] banyak $kata[4]";
+?>
